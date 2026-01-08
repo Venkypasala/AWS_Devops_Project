a99b18c2e83851b93e44835f6c9e71e20f11a990
venkypasala <118915456+Venkypasala@users.noreply.github.com>
2026-01-08 12:46:22 +0530

    Add required providers block for New Relic

--- terraform/Newrelic_parsing/providers.tf
+++ terraform/Newrelic_parsing/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 3.0"
+    }
+  }
+}
 
 provider "newrelic" {
   api_key = var.license_key
